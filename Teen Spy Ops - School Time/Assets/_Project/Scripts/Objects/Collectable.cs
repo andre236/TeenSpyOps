8a81fcb503ddaffbfeef780a91d2dd1e2fa7d594
andre236 <adr_hb@hotmail.com>
2022-07-22 17:57:23 -0300

    Update Collectable

--- Teen Spy Ops - School Time/Assets/_Project/Scripts/Objects/Collectable.cs
+++ Teen Spy Ops - School Time/Assets/_Project/Scripts/Objects/Collectable.cs
@@ -21,13 +21,33 @@ namespace Objects
 
         private Animator _clickOverAnimation;
 
+        [SerializeField] private bool _isRandomValue;
+        [SerializeField] private SkillState _currentTypeObject;
+        [SerializeField] private XRayDistance _currentDistanceHidden;
 
         private ItemConfig _itemConfig;
         [SerializeField] private UnityAction CorrectChoosen;
 
         [field: Header("Where you going find")]
-        [field: SerializeField] public SkillState CurrentTypeObject { get; private set; }
-        [field: SerializeField] public XRayDistance CurrentDistanceHidden { get; private set; }
+        public SkillState CurrentTypeObject
+        {
+            get
+            {
+                if (!_isRandomValue)
+                    return _currentTypeObject;
+                else
+                    return CurrentTypeObject;
+            }
+            set 
+            {
+                if (!_isRandomValue)
+                    _currentTypeObject = value;
+                else
+                    CurrentTypeObject = value;
+            }
+        }
+
+        public XRayDistance CurrentDistanceHidden { get; private set; }
 
         public Action<string, Sprite, Sprite, Sprite, Sprite> GotQuestion;
         public Action<GameObject> CheckedItemOnList;
@@ -50,6 +70,11 @@ namespace Objects
         {
             var gameManager = FindObjectOfType<GameManager>();
 
+            if (_isRandomValue)
+            {
+                CurrentTypeObject = (SkillState)UnityEngine.Random.Range(1, 3);
+                CurrentDistanceHidden = (XRayDistance)UnityEngine.Random.Range(0,2);
+            }
 
             if (CurrentTypeObject == SkillState.XRay)
                 _spriteObject = _itemConfig.SpriteXRayObject;
@@ -82,24 +107,27 @@ namespace Objects
             }
 
             var sortedNumbers = numbersToShuffle.OrderBy(a => Guid.NewGuid()).ToArray();
-            
-            for( int i = 0; i < sortedNumbers.Length; i++)
+
+            for (int i = 0; i < sortedNumbers.Length; i++)
             {
-                if(PlayerPrefs.GetInt(filesInfo[sortedNumbers[i]].Name) == 0)
+                if (PlayerPrefs.GetInt(filesInfo[sortedNumbers[i]].Name) == 0)
                 {
                     _itemConfig = (ItemConfig)AssetDatabase.LoadAssetAtPath(directory + "/" + filesInfo[sortedNumbers[i]].Name, typeof(ItemConfig));
                     PlayerPrefs.SetInt(filesInfo[sortedNumbers[i]].Name, 1);
                     Debug.Log("Registrado o item: " + filesInfo[sortedNumbers[i]].Name);
                     break;
                 }
-                else if(i == sortedNumbers.Length - 1 && PlayerPrefs.GetInt(filesInfo[sortedNumbers[i]].Name) == 1)
+                else if (i == sortedNumbers.Length - 1 && PlayerPrefs.GetInt(filesInfo[sortedNumbers[i]].Name) == 1)
                 {
+                    //Só para não deixar em branco.
+                    _itemConfig = (ItemConfig)AssetDatabase.LoadAssetAtPath(directory + "/" + filesInfo[UnityEngine.Random.Range(0, 3)].Name, typeof(ItemConfig));
+
                     Debug.Log("Todos já foram gerados.");
                 }
             }
-            
 
-          
+
+
         }
 
         private void OnMouseDown()
