4fbbb1ae4d30f915a678b2981027ff9d1a0ab55d
andre236 <adr_hb@hotmail.com>
2022-07-23 11:15:17 -0300

    Update Collectable and LevelManager

--- Teen Spy Ops - School Time/Assets/_Project/Scripts/Managers/LevelManager.cs
+++ Teen Spy Ops - School Time/Assets/_Project/Scripts/Managers/LevelManager.cs
@@ -18,9 +18,9 @@ namespace Manager
 
         [field: SerializeField] public float InitialTimerLevel { get; private set; }
         public float TimerLevel { get; private set; }
+        public GameObject CurrentObject { get; private set; }
         public int ItemsLeft { get; private set; }
         public List<Collectable> ItemsCollectable = new List<Collectable>();
-        public GameObject CurrentObject { get; private set; }
 
         public Action StoppedTimer;
 
@@ -28,17 +28,17 @@ namespace Manager
         {
             _spawnSchoolObject = GameObject.FindGameObjectsWithTag("RespawnObject");
 
-            
-            ItemsCollectable.AddRange(FindObjectsOfType<Collectable>());            
+
+            ItemsCollectable.AddRange(FindObjectsOfType<Collectable>());
         }
 
         internal void OnInitializedLevel()
         {
             int randomIndexObject = UnityEngine.Random.Range(0, _spawnSchoolObject.Length - 1);
-            
+
             for (int i = 0; i < 100; i++)
             {
-                if(_spawnSchoolObject[randomIndexObject].transform.childCount == 0 && _amountInstantiatedSchoolObjects < 3)
+                if (_spawnSchoolObject[randomIndexObject].transform.childCount == 0 && _amountInstantiatedSchoolObjects < 3)
                 {
                     Instantiate(_prefabSchoolObject, _spawnSchoolObject[randomIndexObject].transform);
                     _amountInstantiatedSchoolObjects++;
@@ -100,7 +100,7 @@ namespace Manager
             if (PlayerPrefs.GetInt(string.Concat("STARS", SceneManager.GetActiveScene().name)) < amountStars)
                 PlayerPrefs.SetInt(string.Concat("STARS", SceneManager.GetActiveScene().name), amountStars);
 
-            
+
         }
 
         internal void OnWonGame()
