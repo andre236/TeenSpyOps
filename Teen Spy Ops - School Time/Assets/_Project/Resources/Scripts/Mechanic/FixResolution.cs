042f59c2854fdcd0934e8b29ef18efb86539c8b1
andre236 <adr_hb@hotmail.com>
2023-12-01 19:06:09 -0300

    midufy

--- Teen Spy Ops - School Time/Assets/_Project/Resources/Scripts/Mechanic/FixResolution.cs
+++ Teen Spy Ops - School Time/Assets/_Project/Resources/Scripts/Mechanic/FixResolution.cs
@@ -1,17 +1,49 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FixResolution : MonoBehaviour
 {
     [SerializeField] private float _orthoSize = 5f;
     [SerializeField] private float _aspect = 1.66f;
 
+    private List<CanvasScaler> _allCanvasScaler;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        Camera.main.projectionMatrix = Matrix4x4.Ortho(-_orthoSize * _aspect, _orthoSize * _aspect, -_orthoSize, _orthoSize, Camera.main.nearClipPlane, Camera.main.farClipPlane);
+        _allCanvasScaler = new List<CanvasScaler>();
+        _allCanvasScaler.AddRange(FindObjectsOfType<CanvasScaler>());
     }
 
+    void Update()
+    {
+        //Modify();
+        RefreshAllCanvas();
+    }
+
+    private void RefreshAllCanvas()
+    {
+
+        foreach (CanvasScaler canvas in _allCanvasScaler)
+        {
+            if (Screen.width > 768)
+                canvas.matchWidthOrHeight = 1;
+            else
+                canvas.matchWidthOrHeight = 0;
+
+        }
+
+
+
+
+    }
+
+    private void Modify()
+    {
+        Camera.main.projectionMatrix = Matrix4x4.Ortho(-_orthoSize * _aspect, _orthoSize * _aspect, -_orthoSize, _orthoSize, Camera.main.nearClipPlane, Camera.main.farClipPlane);
+
+    }
 
 
 }
